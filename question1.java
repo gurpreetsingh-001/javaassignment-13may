d4cc8f95bb5198cbec704b9139ca4cf8017758bf
unknown <gurpreetsingh@hatchconcepts.in>
2023-05-18 17:45:02 +0530

    Graded Assignment on Introduction to Java

---
+++ question1.java
@@ -0,0 +1,18 @@
+public class question1 {
+    
+    public static void main(String[] args) {
+        int num1=10;
+        int num2=20;
+
+        if(num1!=num2)
+        {
+            System.out.println("Equal: False");
+            System.out.println("Not Equal: True");
+        } 
+        if(num1<num2)
+        {
+            System.out.println("num1 is Greater Than num2: False");
+            System.out.println("num1 is Less Than num2: True");
+        }
+    }
+}
---
+++ question2.java
@@ -0,0 +1,17 @@
+public class question2 {
+    
+    public static void main(String[] args) {
+        int num1=10;
+
+        num1+=2;
+        System.out.println("After Addition num= " +num1);
+        num1-=4;
+        System.out.println("After Subtraction num= " +num1);
+        num1*=7;
+        System.out.println("After Multiplication num= " +num1);
+        num1/=4;
+        System.out.println("After Division num= " +num1);
+        num1%=4;
+        System.out.println("After Modulus num= " +num1);
+    }
+}
